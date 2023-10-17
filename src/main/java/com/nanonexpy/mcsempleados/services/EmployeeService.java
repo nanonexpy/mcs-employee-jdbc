ebd927def054ed16f79b90ecf9dac542e889d586
nanonexpy <105869103+nanonexpy@users.noreply.github.com>
2023-10-17 10:19:32 -0300

    Crea el servicio para llamar a todos los empleados

--- src/main/java/com/nanonexpy/mcsempleados/services/EmployeeService.java
+++ src/main/java/com/nanonexpy/mcsempleados/services/EmployeeService.java
@@ -1,14 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 package com.nanonexpy.mcsempleados.services;
+
+import com.nanonexpy.mcsempleados.models.Employee;
+import com.nanonexpy.mcsempleados.models.rowMapper.EmployeeRowMapper;
+import java.util.List;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Service;
 
 /**
  *
  * @author nanonexpy
  */
-public class EmployeeService {
 
+@Service
+public class EmployeeService {
+    private final NamedParameterJdbcTemplate template;
+
+    public EmployeeService(NamedParameterJdbcTemplate template) {
+        this.template = template;
+    }
+    
+    //Consulta SQL
+    public List<Employee> getAllEmployees(){
+        String consulta = "SELECT * FROM employees";
+        return template.query(consulta, new EmployeeRowMapper());
+    } 
 }
