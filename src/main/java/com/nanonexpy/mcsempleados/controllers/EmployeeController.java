f1c70e2f6b927c87ca8d5c2e1af4053836f75569
nanonexpy <105869103+nanonexpy@users.noreply.github.com>
2023-10-17 10:20:13 -0300

    Implementa el servicio y devuelve una lista de empleados

--- src/main/java/com/nanonexpy/mcsempleados/controllers/EmployeeController.java
+++ src/main/java/com/nanonexpy/mcsempleados/controllers/EmployeeController.java
@@ -1,7 +1,9 @@
 package com.nanonexpy.mcsempleados.controllers;
 
 import com.nanonexpy.mcsempleados.models.Employee;
+import com.nanonexpy.mcsempleados.services.EmployeeService;
 import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,8 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/empleados")
 public class EmployeeController {
+    private final EmployeeService service;
     
+    public EmployeeController(EmployeeService service){
+        this.service = service;
+    }
+    
+    @GetMapping
     public List<Employee> listarEmpleados() {
-        return null;
+        return service.getAllEmployees();
     }
 }
