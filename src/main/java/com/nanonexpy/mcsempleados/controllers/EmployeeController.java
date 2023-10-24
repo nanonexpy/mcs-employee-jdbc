f6dbb92ac9fc00c51ddd23216e830088521526c6
nanonexpy <105869103+nanonexpy@users.noreply.github.com>
2023-10-24 16:18:32 -0300

    Agrega el ResponseEntity al controller de empleados

--- src/main/java/com/nanonexpy/mcsempleados/controllers/EmployeeController.java
+++ src/main/java/com/nanonexpy/mcsempleados/controllers/EmployeeController.java
@@ -3,6 +3,7 @@ package com.nanonexpy.mcsempleados.controllers;
 import com.nanonexpy.mcsempleados.models.Employee;
 import com.nanonexpy.mcsempleados.services.EmployeeService;
 import java.util.List;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,7 +23,12 @@ public class EmployeeController {
     }
     
     @GetMapping
-    public List<Employee> listarEmpleados() {
-        return service.getAllEmployees();
+    public ResponseEntity<List<Employee>> listarEmpleados() {
+        List<Employee> employees = service.getAllEmployees();
+        if (employees.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.ok(employees);
+        }
     }
 }
