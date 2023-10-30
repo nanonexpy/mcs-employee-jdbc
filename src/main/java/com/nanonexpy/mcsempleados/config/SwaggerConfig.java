c5d6c1ab249a04334d6d52a4cdc8b215224999db
nanonexpy <105869103+nanonexpy@users.noreply.github.com>
2023-10-30 14:25:52 -0300

    Agrega Swagger y ajustes menores

---
+++ src/main/java/com/nanonexpy/mcsempleados/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.nanonexpy.mcsempleados.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+/**
+ *
+ * @author nanonexpy
+ */
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.nanonexpy.mcsempleados.controllers"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
--- target/classes/schema.sql
+++ target/classes/schema.sql
@@ -4,7 +4,7 @@
  */
 
 CREATE TABLE employees (
-    id INT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(255),
     last_name VARCHAR(255),
     document_number VARCHAR(20),
